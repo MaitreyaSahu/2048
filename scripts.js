1ae376fc3b44216e9f263b054dd8aa3b14bb4ec1
Maitreya <mtsahu1995@gmail.com>
2021-04-24 15:07:52 +0530

    pwa support

--- scripts.js
+++ scripts.js
@@ -423,3 +423,19 @@ mc.on("swipeup", function () {
 mc.on("swipedown", function () {
     moveDown();
 });
+
+
+if('serviceWorker' in navigator) {
+    console.log(window);
+    
+        navigator.serviceWorker.register('sw.js')
+            .then((registration) => { 
+                console.log(`service worker registered succesfully ${registration}`) 
+            })
+            .catch((err) => {
+                console.log(`Error registring ${err}`) 
+            })
+   
+} else {
+    console.log(`Service worker is not supported in this browser.`)
+}
